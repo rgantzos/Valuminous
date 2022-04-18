4814a1f2d0361662e89e63f18e5ef1b003991ccb
Electro-Corp <chinmaytanmay@outlook.com>
2022-04-18 17:08:16 -0600

    Update getdata.js

--- getdata.js
+++ getdata.js
@@ -1,5 +1,5 @@
 var request = new XMLHttpRequest(); 
-request.open("GET", 'https://raw.githubusercontent.com/Scratchfangs/Valuminous/main/rsz_12021-08-12-ts3_thumbs-9cf.png ');
+request.open("GET", 'https://raw.githubusercontent.com/Scratchfangs/Valuminous/main/projects/scraft.html');
 request.onreadystatechange = function() { 
 if (request.readyState === 4 && request.status === 200) {
   htmldata = xhttp.responseText;

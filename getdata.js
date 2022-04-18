35158b667980871c02be69e975f4988c3ec460bb
Electro-Corp <chinmaytanmay@outlook.com>
2022-04-18 17:05:02 -0600

    Update getdata.js

--- getdata.js
+++ getdata.js
@@ -2,9 +2,8 @@ var request = new XMLHttpRequest();
 request.open("GET", 'https://raw.githubusercontent.com/Scratchfangs/Valuminous/main/rsz_12021-08-12-ts3_thumbs-9cf.png ');
 request.onreadystatechange = function() { 
 if (request.readyState === 4 && request.status === 200) {
-  data = xhttp.responseText;
-  img = "<img src="+data+"alt="win11 logo or something">";
-  document.getElementById("test").src="https://raw.githubusercontent.com/Scratchfangs/Valuminous/main/rsz_12021-08-12-ts3_thumbs-9cf.png";
+  htmldata = xhttp.responseText;
+  document.getElementById("test").innerHTML=htmldata;
 
 }
 };
